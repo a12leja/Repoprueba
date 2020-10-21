505af0a540b7b43b73dd7763d0cc881da6aeb466
a12leja <63980597+a12leja@users.noreply.github.com>
2020-10-20 23:12:06 -0500

    Add files via upload

---
+++ compunube/parcial2/punto2/comandos.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/kubernetes/examples/master/guestbook/all-in-one/guestbook-all-in-one.yaml
+kubectl apply -f guestbook-all-in-one.yaml
---
+++ compunube/parcial2/punto3/comandos.sh
@@ -0,0 +1,9 @@
+mkdir clouddrive/punto3
+------------------------
+curl -O https://raw.githubusercontent.com/Azure-Samples/azure-voting-app-redis/master/azure-vote-all-in-one-redis.yaml
+------------------------------
+kubectl create -f azure-vote-all-in-one-redis.yaml
+--------------------------
+kubectl get pods
+----------------------------
+kubectl get svc
